c5149f49fb335ced8f9d53611e496dad2fd96138
Abdi Bacha <contactabdi.bacha@gmail.com>
2024-04-19 12:52:44 +0300

    first commit

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+
+
+app.get('/login', (req, res)=>{
+    res.send({message: "message already sent"});
+})
+
+app.listen(process.env.PORT || 2000 , ()=>{
+    console.log(`Listening on the port ${process.env.PORT}`)
+});
